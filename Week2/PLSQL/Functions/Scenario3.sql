9a5b712c94562ece856d449d5428f706ad9bb66a
Sakthi Alagappan <122151829+Alsa5@users.noreply.github.com>
2024-08-07 14:03:44 +0530

    Create Scenario3.sql

---
+++ Week2/PLSQL/Functions/Scenario3.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION HasSufficientBalance(
+  p_account_id NUMBER,
+  p_amount NUMBER
+) RETURN BOOLEAN IS
+  v_balance NUMBER;
+BEGIN
+  -- Fetch the balance of the account
+  SELECT balance INTO v_balance
+  FROM accounts
+  WHERE account_id = p_account_id;
+  
+  -- Check if the balance is sufficient
+  IF v_balance >= p_amount THEN
+    RETURN TRUE;
+  ELSE
+    RETURN FALSE;
+  END IF;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    RETURN FALSE;
+  WHEN OTHERS THEN
+    RETURN FALSE;
+END HasSufficientBalance;
+/
