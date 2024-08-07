11b67bdc3f293e2b5732316627ab788de8eef35d
Sakthi Alagappan <122151829+Alsa5@users.noreply.github.com>
2024-08-07 13:54:56 +0530

    Update Scenario2.sql

--- Week2/PLSQL/ControlStructures/Scenario2.sql
+++ Week2/PLSQL/ControlStructures/Scenario2.sql
@@ -1,1 +1,23 @@
-
+DECLARE
+  CURSOR c_customers IS
+    SELECT customer_id, balance
+    FROM customers
+    WHERE balance > 10000;
+    
+  v_customer_id customers.customer_id%TYPE;
+  v_balance customers.balance%TYPE;
+BEGIN
+  OPEN c_customers;
+  LOOP
+    FETCH c_customers INTO v_customer_id, v_balance;
+    EXIT WHEN c_customers%NOTFOUND;
+    
+    -- Set IsVIP flag to TRUE
+    UPDATE customers
+    SET IsVIP = TRUE
+    WHERE customer_id = v_customer_id;
+  END LOOP;
+  CLOSE c_customers;
+  
+  COMMIT;
+END;
