cb253e3ebe1700d8c7d0e50a77073f22a900f340
Sakthi Alagappan <122151829+Alsa5@users.noreply.github.com>
2024-08-07 14:17:04 +0530

    Create PackageSpec.sql

---
+++ Week2/PLSQL/Packages/Scenario3/PackageSpec.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE PACKAGE AccountOperations AS
+  PROCEDURE OpenAccount(
+    p_account_id IN NUMBER,
+    p_customer_id IN NUMBER,
+    p_balance IN NUMBER
+  );
+  
+  PROCEDURE CloseAccount(
+    p_account_id IN NUMBER
+  );
+  
+  FUNCTION GetTotalBalance(
+    p_customer_id IN NUMBER
+  ) RETURN NUMBER;
+END AccountOperations;
+/
